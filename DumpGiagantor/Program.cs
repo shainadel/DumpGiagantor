b55a65d5cdbc9d8ecccfc7a2e0d2d1804e1df2c6
Felix Fainshtein <Felix.Fainshtein@cellebrite.com>
2020-06-28 15:25:33 +0300

    Added Message From Felix's Branch

--- DumpGiagantor/Program.cs
+++ DumpGiagantor/Program.cs
@@ -7,6 +7,7 @@ namespace DumpGiagantor
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Felix Was Here!");
         }
     }
 }
